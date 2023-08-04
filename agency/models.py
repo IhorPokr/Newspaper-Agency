cb8ca4fbcf871b4b61eee1e53e9b9d47b5928f0a
Ihor Pokrovetskyi <pokroveckijigor@gmail.com>
2023-08-03 20:22:10 -0500

    Added redactor, topic, newspaper models and migrated

--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,38 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ["username"]
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    publisher_date = models.DateField()
+    topic = models.ForeignKey(
+        Topic,
+        on_delete=models.CASCADE,
+        related_name="newspapers"
+    )
+    publishers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="newspapers")
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return f"{self.title} {self.content} (date: {self.publisher_date}, topic: {self.topic})"
