b6836261ac21bdd8775a1ac954e04c4cde2b8916
Ihor Pokrovetskyi <pokroveckijigor@gmail.com>
2023-08-04 11:19:08 -0500

    Hided SECRET_KEY in setting.py

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^x6lqjr*=scsp3tz!8)#i&#w*bjiz$^9zt3(f*=@a9xcv==#+@"
+load_dotenv()
+
+
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
