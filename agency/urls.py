9e701b9f0232e59c7893b4b8371c601c35d01dff
Ihor Pokrovetskyi <pokroveckijigor@gmail.com>
2023-08-07 14:00:36 -0500

    Added redactors newspapers newspapers/<int:pk> and topics to urlpatterns in agency/urls.py

--- agency/urls.py
+++ agency/urls.py
@@ -1,9 +1,29 @@
 from django.urls import path
 
-from agency.views import index
+from agency.views import index, RedactorListView, NewspaperListView, TopicListView, NewspaperDetailView
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "redactors/",
+        RedactorListView.as_view(),
+        name="redactor-list"
+    ),
+    path(
+        "newspapers/",
+        NewspaperListView.as_view(),
+        name="newspaper-list"
+    ),
+    path(
+        "newspapers/<int:pk>",
+        NewspaperDetailView.as_view(),
+        name="newspaper-detail"
+    ),
+    path(
+        "topics/",
+        TopicListView.as_view(),
+        name="topic-list"
+    )
 ]
 
 app_name = "agency"
