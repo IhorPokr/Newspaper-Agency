018b2e86080f188c389ef37c2acf1d23fb55ecf1
Ihor Pokrovetskyi <pokroveckijigor@gmail.com>
2023-08-09 12:38:20 -0500

    Added num_visits to views.py

--- agency/views.py
+++ agency/views.py
@@ -9,10 +9,14 @@ def index(request):
     num_redactors = Redactor.objects.count()
     num_topics = Topic.objects.count()
 
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
     context = {
         "num_newspapers": num_newspapers,
         "num_redactors": num_redactors,
         "num_topics": num_topics,
+        "num_visits": num_visits + 1,
     }
 
     return render(request, "agency/index.html", context=context)
