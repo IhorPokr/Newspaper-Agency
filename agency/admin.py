affc15c75c74faa41f67718854bbcaa49519c5be
Ihor Pokrovetskyi <pokroveckijigor@gmail.com>
2023-08-12 10:06:48 -0500

    Customized admin panel in admin.py

--- agency/admin.py
+++ agency/admin.py
@@ -1,7 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from agency.models import Redactor, Topic, Newspaper
 
-admin.site.register(Redactor)
+
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("years_of_experience",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "years_of_experience",
+                    )
+                },
+            ),
+        )
+    )
+
+
 admin.site.register(Topic)
-admin.site.register(Newspaper)
+
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    search_fields = ("title",)
+    list_filter = ("topic",)
