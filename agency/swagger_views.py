b61e34012256946c1b37675f9afb9ab9f39721d7
Ihor Pokrovetskyi <pokroveckijigor@gmail.com>
2023-08-30 13:34:51 -0500

    Remade with rest_framework_swagger in swagger_views.py

--- agency/swagger_views.py
+++ agency/swagger_views.py
@@ -1,16 +1,3 @@
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Newspaper Agency API",
-        default_version='v1',
-        description="API documentation for the Newspaper Agency service.",
-        terms_of_service="https://www.agency.com/terms/",
-        contact=openapi.Contact(email="contact@agency.com"),
-        license=openapi.License(name="Your License"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
-)
+schema_view = get_swagger_view(title="Newspaper Agency API Documentation")
